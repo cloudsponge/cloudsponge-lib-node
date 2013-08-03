c99def0c18819828def52bf5f899a955c6368920
Hector <hectorbavio@gmail.com>
2013-08-03 19:18:09 -0300

    - Add contact constructor.

--- example.js
+++ example.js
@@ -2,8 +2,8 @@ browser     = require('openurl');
 cloudsponge = require('./cloudsponge');
 
 AUTHENTICATION = {domain_key: 'RN2UKFNXLW4RTATJHDVU', domain_password: 'qiz6wNmTde0FjI7'};
-cloudsponge.Importer.authorize('gmail', AUTHENTICATION, function(resp){
-  console.log('response', resp);
-  // browser.open(resp.url);
-  // res.beginImport();
+cloudsponge.Importer.authorize('gmail', AUTHENTICATION, function(contacts){
+  browser.open(contacts.url);
+  contacts.get();
 });
+
