a1535af05d6f76e1ff7caa4e39295634303f819f
Hector <hectorbavio@gmail.com>
2013-08-03 21:11:28 -0300

    - Moar refactor.

--- example.js
+++ example.js
@@ -2,8 +2,14 @@ browser     = require('openurl');
 cloudsponge = require('./cloudsponge');
 
 AUTHENTICATION = {domain_key: 'RN2UKFNXLW4RTATJHDVU', domain_password: 'qiz6wNmTde0FjI7'};
-cloudsponge.Importer.authorize('gmail', AUTHENTICATION, function(contacts){
-  browser.open(contacts.url);
-  contacts.get();
+cloudsponge.Importer.authorize('gmail', AUTHENTICATION, function(response){
+  browser.open(response.contacts.consent_url);
+  response.events.get()
+  // resp = contacts.get();
+  // res
+  // while(true){
+
+  // }
+
 });
 
