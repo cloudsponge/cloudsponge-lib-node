ad5c2638ee836f00a25ff0e3af5c1b0654d182a9
Hector <hectorbavio@gmail.com>
2013-08-17 15:13:30 -0300

    - Import with emitter pattern.

--- cloudsponge.js
+++ cloudsponge.js
@@ -1,1 +1,2 @@
-exports.Importer = require("./lib/importer");
+// exports.Importer = require("./lib/importer");
+exports.Importer = require("./lib/emit_importer");
--- example.js
+++ example.js
@@ -1,19 +1,35 @@
-browser     = require('openurl'); 
-cloudsponge = require('./cloudsponge');
+var browser     = require('openurl'); 
+var cloudsponge = require('./cloudsponge');
 
-AUTHENTICATION = {domain_key: 'YRMBHYC3DRPEK3LUSCYK', domain_password: 'zgIER11nFCWMAGM'};
+var AUTHENTICATION = {domain_key: 'YRMBHYC3DRPEK3LUSCYK', domain_password: 'zgIER11nFCWMAGM'};
 
-cloudsponge.Importer.authorize('gmail', AUTHENTICATION, function(response){
-  console.log("open browser at:", response.contacts.consent_url)
+// cloudsponge.Importer.authorize('gmail', AUTHENTICATION, function(response){
+//   console.log("open browser at:", response.contacts.consent_url)
 
-  browser.open(response.contacts.consent_url);
-  response.events.get(function() {
-    response.contacts.get(function(collection){
-      for (var key in collection) {
-        if (collection.hasOwnProperty(key)) {
-          console.log(collection[key])
-        }
-      }
-    });
+//   browser.open(response.contacts.consent_url);
+//   response.events.get(function() {
+//     response.contacts.get(function(collection){
+//       console.log("OK")
+//       // for (var key in collection) {
+//       //   if (collection.hasOwnProperty(key)) {
+//       //     console.log(collection[key])
+//       //   }
+//       // }
+//     });
+//   });
+// });
+
+
+// console.log(cloudsponge.Importer);
+// cloudsponge.Importer.on('BeginImport')
+var importer = cloudsponge.Importer;
+
+importer.import("gmail", AUTHENTICATION);
+
+importer.on('Initialized', function(consent){
+  browser.open(consent.url);
+  importer.consent.getEvents(consent.import_id, function(contacts){
+    console.log("rtn:::", contacts);
+
   });
 });
